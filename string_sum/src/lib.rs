6eb56b88f98b231b5779ac6e8742c639cf120007
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-06 09:41:03 +0100

    add example

--- string_sum/src/lib.rs
+++ string_sum/src/lib.rs
@@ -6,12 +6,18 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Multiply two numbers.
+#[pyfunction]
+fn multiply(a: usize, b: usize) -> PyResult<usize> {
+    Ok(a * b)
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
 fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
+    m.add_function(wrap_pyfunction!(multiply, m)?)?;
     Ok(())
 }
