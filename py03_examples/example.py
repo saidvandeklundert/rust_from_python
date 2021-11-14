3b78b924c347a8e613acdc6079bd2e3dd1da8ff8
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-14 21:26:12 +0100

    remove

--- py03_examples/example.py
+++
@@ -1,9 +0,0 @@
-import pyo3_examples
-
-
-if __name__ == "__main__":
-    x = pyo3_examples.sum_as_string(5, 20)
-    print(x)
-    x = pyo3_examples.multiply(5, 20)
-    print(x)
-    print(pyo3_examples.list_sum([10, 10, 10, 10, 10, 5]))
